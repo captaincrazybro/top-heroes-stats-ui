cfe1582ddab87942de33feb45d270831501a31fc
Levi Wenger <myeyesareonmizar@gmail.com>
2026-06-14 01:42:36 -0400

    fix: removed base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 
-const base = process.env.BASE_PATH ?? '';
-
 export default {
   kit: {
     adapter: adapter({
       fallback: 'index.html'
     }),
-    paths: { base }
   }
 };
